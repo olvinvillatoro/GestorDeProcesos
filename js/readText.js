01d3e8f8a9bc3543f00d2a00f06317f6de5bfaca
4nG3L000 <MiGu3L_4nG3L@outlook.com>
2020-04-23 16:03:22 -0600

    4nG3L000
    Tabla de procesos y nuevo proceso (sin agregar al txt).

--- js/readText.js
+++ js/readText.js
@@ -15,11 +15,24 @@ input.addEventListener
             const reader = new FileReader();
             reader.onload = ()=>{
                 proceso.procesos = reader.result.split(';');
-                console.log(proceso.procesos);
-                // prueba de como llamar la funcion para separar el proceso en instrucciones
-               const instr = separarInstrucciones(proceso.procesos[0]);
+                //console.log(proceso.procesos);
 
-               console.log('final ' +instr.instruccion);
+                //llamar la funcion para separar el proceso en instrucciones y se crea la tabla de procesos
+                for (var i = 0; i < proceso.procesos.length; i++) {
+                   
+                     document.getElementById("procesos").innerHTML += "<tr id='proceso"+i+"'></tr>";
+
+                     const instr = separarInstrucciones(proceso.procesos[i]);
+                     for (var k = 0; k < instr.instruccion.length; k++) {
+
+                             var p = "proceso"+i+"";
+                            document.getElementById(""+p+"").innerHTML += "<td>"+instr.instruccion[k]+"</td>";
+
+                        }
+                     //console.log('final' +instr.instruccion);
+                }
+               //
+              
                 }
         reader.readAsText(input.files[0])
         },
@@ -36,3 +49,35 @@ input.addEventListener
         
          return instrucciones;
    }
+//sta funcion crea un nuevo proceso en la lista de procesos
+function crearProceso () {
+    if (proceso.procesos.length <= 9) {
+        document.getElementById("procesos").innerHTML = "";
+        console.clear();
+
+        var prcs =  Math.round( Math.random() * (3000-1000)+1000)+"/"+//id
+                    Math.round( Math.random() * 5)+"/"+//estado
+                    Math.round( Math.random() * 3)+"/"+//prioridad
+                    Math.round( Math.random() * (100-50))+"/"+//instrucciones
+                    Math.round( Math.random() * (50-10))+"/"+//bloqueo
+                    Math.round( Math.random() * (5-2));//evento
+
+        proceso.procesos.push(prcs);   
+                 
+        for (var i = 0; i < proceso.procesos.length; i++) {
+                       
+                 document.getElementById("procesos").innerHTML += "<tr id='proceso"+i+"'></tr>";
+
+                 const instr = separarInstrucciones(proceso.procesos[i]);
+
+                 for (var k = 0; k < instr.instruccion.length; k++) {
+
+                         var p = "proceso"+i+"";
+                        document.getElementById(""+p+"").innerHTML += "<td>"+instr.instruccion[k]+"</td>";
+
+                }
+        }
+    }else{
+        alert("solo se permiten 10 procesos");
+    }
+}
