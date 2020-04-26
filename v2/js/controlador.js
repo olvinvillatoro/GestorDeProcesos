7e392448626acff9bf891a89d384a8b620adc025
Edwin Quintero <EdwinQuintero01246>
2020-04-26 15:57:50 -0600

    coomits

--- v2/js/controlador.js
+++ v2/js/controlador.js
@@ -1,7 +1,10 @@
 function cargarProcesos(){
+	var parametros = 'NombreProceso='+$("#Procesos").val();
 
 	$.ajax({
-		url: "ajax/acciones.php?accion=2",//aqui se manda accion=2 a acciones php mediante GET
+		url: "ajax/acciones.php?accion=ListarProcesosDeUnBCP",//aqui se manda accion=2 a acciones php mediante GET
+		data: parametros,
+		method: "POST",
 		dataType: 'json',
 		success:function(resultado){
 			//aqui vacia la tabla 
@@ -23,7 +26,7 @@ function cargarProcesos(){
 				}
 
 				ciclos(resultado);
-				nuevos();//esta llamada solo llena la columna de procesos nuevos a la vez que se llena la tabla
+				nuevos($("#Procesos").val());//esta llamada solo llena la columna de procesos nuevos a la vez que se llena la tabla
 
 			},
 		error:function(){
@@ -34,20 +37,86 @@ function cargarProcesos(){
 }
 //esta funcion va cambiando el estado de los procesos y actualizando los datos de las tablas
 function ciclos(resultado){
-
+	var nCiclos = 0;
+	var nSegmentos = 1;
+	var eS = 0;
+	var hDD = 0;
 	var cant = $("#ciclos").val();//valor de la caja de texto (cantidad de ciclos)
 
 	(function ciclo(i) {//esta hace que los ciclos sea lentos para poder ver los cambios
 	  setTimeout(function() {
-	    
+
+
+	   if (nCiclos == 5) {//contador de ciclos y segmentos
+	  		nCiclos = 1;
+	  		nSegmentos = parseInt(nSegmentos)+1;
+	  	}else{
+	  		 nCiclos = parseInt(nCiclos)+1; 
+	  	}
+
+		$("#mostrarCiclos").html("Ciclo:"+parseInt(nCiclos)+"  "+"Segmento:"+parseInt(nSegmentos)); //muestra los ciclos en pantalla 
+
+	    //console.log("Ciclo:"+nCiclos);
+	    //console.log("segmento:"+nSegmentos);
 	    for (var j = 0; j < resultado.length; j++) {//aqui se cambian los estados
-	    	/*en este if se genera un valor de 0 a 100 y si es mayor que 50
-	    	el estado cambia delo contrario se mantiene en el mismo estado*/
-			if (Math.floor(Math.random() * 101) > 50) {
-				resultado[j].estado = parseInt(resultado[j].estado)+1;
+	    	
+	    	if (resultado[j].estado==2) {//este if simula la  ejecucion de las instrucciones
+				//se restan 1 instrucciones por ciclo al total de instrucciones de cada proceso;
+				resultado[j].cantidad = parseInt(resultado[j].cantidad)-1;
+				if (resultado[j].cantidad<1) {
+					resultado[j].cantidad = 0;//quedan cero instrucciones del total
+				}
 			}
 
+			if (resultado[j].estado==3) {//conteo de ciclos mientras el proceso esta boqueado
+				if (eS < 13) {//para evento E/S el proceso se bloquea durante 13 ciclos
+					eS = parseInt(eS)+1;
+				}
+				if (hDD < 27) {//para evento HDD el proceso se bloquea durante 27 ciclos
+					hDD = parseInt(hDD)+1;
+				}
+				
+				console.log(eS+" "+hDD)
+			}
+
+	    	/*en este if se genera un valor de 0 a 100 y si es mayor que 50 y han pasado 5 ciclos
+	    	el estado cambia delo contrario se mantiene en el mismo estado*/
+			//if (Math.floor(Math.random() * 101) > 50) {
+				if (resultado[j].estado<=3 && nCiclos==5) {//se encarga de que el estado se 4 como maximo
+
+					if (resultado[j].estado==2) {//cambia el estado de un proceso en ejecucion (bloqueado o terminado)
+						if (resultado[j].cantidad < 1){//si cantidad de instrcciones es cero = proceso terminado (estado = 4)
+							resultado[j].estado = parseInt(resultado[j].estado)+2;
+						}else{//probabilidad de que se bloquee por esperar un evento
+							if (resultado[j].cantidad == resultado[j].bloqueo && resultado[j].evento != 4) {
+							resultado[j].estado = parseInt(resultado[j].estado)+1;
+							}
+						}
+					}else{
+						if (resultado[j].estado==3) {//eventos para desbloquear procesos
+							if (resultado[j].evento == 3 && eS == 13) {//evento E/S y trancurridos 13 ciclos
+								resultado[j].estado = parseInt(resultado[j].estado)-1;
+								es = 0;//reinicia el contador de ciclos de bloqueo de E/S
+							}
+							if (resultado[j].evento == 5 && hDD == 27) {//evento HDD y trancurridos 27 ciclos
+								resultado[j].estado = parseInt(resultado[j].estado)-1;
+								hDD = 0;//reinicia el contador de ciclos de bloqueo de HDD
+							}
+
+						}else{//prioridades para cambiar de estado
+
+							if (Math.floor(Math.random() * 101) > 50) {
+								resultado[j].estado = parseInt(resultado[j].estado)+1;	
+							} 
+							
+						}
+					}
+				}
+
+	
+		//}
 		}
+
 
 		$("#procesos").html("");//se vacia la tabla principal 
 
@@ -70,31 +139,18 @@ function ciclos(resultado){
 			}
 			              
 	    if (--i) ciclo(i);   
-	  }, 3000)
+	  }, 200)
 	})(cant); 
 }                  
 
-//al presionar el boton "Nuevo" se llama esta funcion para agregar un procesos mas al archivo txt
-function crearProceso(){
 
+//funcion que carga los procesos con estado = 0
+function nuevos(nombre){
+	var datos = 'NombreProceso='+nombre;
 	$.ajax({
-		url: "ajax/acciones.php?accion=1",//accion=1 se evia mediante GET a acciones.php 
-	
-		success:function(resultado){
-
-				cargarProcesos(); //se recarga la tabla principal para agregar el nuevo proceso
-
-			},
-		error:function(){
-			alert("error");
-		}
-	});
-}
-//funcion que carga los procesos con estado = 0 al presionar el boton "cargar"
-function nuevos(){
-
-	$.ajax({
-		url: "ajax/acciones.php?accion=2",
+		url: "ajax/acciones.php?accion=ListarProcesosDeUnBCP",
+		data: datos,
+		method: "POST",
 		dataType: 'json',
 		success:function(resultado){
 				$("#procesoNuevo").html("");
@@ -213,10 +269,11 @@ $(document).ready(function(){
 	});
 	document.getElementById("Procesos").innerHTML = "";
 })
+
 function GenerarProcesosTablas(){
 	var parametros =
 	'NombreProceso='+$("#Procesos").val();
-	$.ajax({
+	/*$.ajax({
 		url: "ajax/acciones.php?accion=ListarBCP",
 		dataType:"json",
         //data:parametros,
@@ -244,7 +301,7 @@ function GenerarProcesosTablas(){
 				}
 			}
 		},
-	});
+	});*/
 	$.ajax({
 		url: "ajax/acciones.php?accion=ListarProcesosDeUnBCP",
 		dataType:"json",
